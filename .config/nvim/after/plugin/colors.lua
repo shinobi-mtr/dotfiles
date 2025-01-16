dd42487ea7684ea919ed9da7857b777d4082f112
shinobi-mtr <education.sultan.mo@gmail.com>
2025-01-16 15:43:12 +0300

    fix: change configurations to .config folder

---
+++ .config/nvim/after/plugin/colors.lua
@@ -0,0 +1,5 @@
+vim.cmd[[colorscheme tokyonight-moon]]
+
+vim.api.nvim_set_hl(0, "Normal", { bg= "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg= "none" })
+
---
+++ .config/nvim/lua/pm/init.lua
@@ -0,0 +1,28 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+
+  use 'wbthomason/packer.nvim'
+  
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.8',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use {
+	  "folke/tokyonight.nvim",
+	  lazy = false,
+	  priority = 1000,
+	  opts = {}
+  }
+
+  use ("nvim-treesitter/nvim-treesitter", { run= ":TSUpdate" })
+
+  use ("mbbill/undotree")
+
+  use({'neovim/nvim-lspconfig'})
+  use({'hrsh7th/nvim-cmp'})
+  use({'hrsh7th/cmp-nvim-lsp'})
+  
+end)
+
