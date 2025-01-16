066257f0d3f96b7ceb219adad4737c1c9dd59f82
shinobi-mtr <education.sultan.mo@gmail.com>
2025-01-16 17:00:58 +0300

    add: make nvim more confin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,13 @@
 require("keys")
 require("pm")
 
+vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.splitbelow = true
+vim.opt.splitright = ture
+vim.opt.wrap = false
+vim.opt.expandtab = ture
+vim.opt.tabstop = 4
+vim.opt.clipboard = "unnamedplus"
+vim.opt.scrolloff = 999
 
