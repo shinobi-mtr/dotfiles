d5cbdc4e389a21794b92401a581e0ee38427d539
shinobi-mtr <education.sultan.mo@gmail.com>
2025-05-10 15:25:20 +0300

    add mocha to nvim

--- .config/nvim/lua/sultan/plugins/colors.lua
+++ .config/nvim/lua/sultan/plugins/colors.lua
@@ -1,9 +1,7 @@
-return {
-	"folke/tokyonight.nvim",
-	lazy = false,
-	priority = 1000,
-	opts = {},
+return { 
+	"catppuccin/nvim",
+	name = "catppuccin", priority = 1000,
 	config = function()
-		vim.cmd.colorscheme("tokyonight-night")
-	end
+		vim.cmd.colorscheme "catppuccin-mocha"
+	end,
 }
