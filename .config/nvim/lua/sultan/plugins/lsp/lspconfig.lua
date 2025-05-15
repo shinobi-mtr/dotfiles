5cf5425e83874b7aa92a03571ddf3ff111350290
shinobi-mtr <education.sultan.mo@gmail.com>
2025-05-15 13:18:32 +0300

    update mason for nvim and waybar configuration

--- .config/nvim/lua/sultan/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/sultan/plugins/lsp/lspconfig.lua
@@ -12,7 +12,6 @@ return {
 
 		-- import mason_lspconfig plugin
 		local mason_lspconfig = require("mason-lspconfig")
-
 		-- import cmp-nvim-lsp plugin
 		local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
@@ -77,43 +76,5 @@ return {
 			local hl = "DiagnosticSign" .. type
 			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 		end
-
-		mason_lspconfig.setup_handlers({
-			-- default handler for installed servers
-			function(server_name)
-				lspconfig[server_name].setup({
-					capabilities = capabilities,
-				})
-			end,
-			["lua_ls"] = function()
-				-- configure lua server (with special settings)
-				lspconfig["lua_ls"].setup({
-					capabilities = capabilities,
-					settings = {
-						Lua = {
-							-- make the language server recognize "vim" global
-							diagnostics = {
-								globals = { "vim" },
-							},
-							completion = {
-								callSnippet = "Replace",
-							},
-						},
-					},
-				})
-			end,
-			["elixirls"] = function()
-				lspconfig["elixirls"].setup({
-					cmd = {vim.fn.stdpath("data") .. "/mason/bin/elixir-ls"},
-					capabilities = capabilities,
-					settings = {
-						elixirLS = {
-							dialyzerEnabled = false,
-							fetchDeps = false
-						}
-					}
-				})
-			end
-		})
 	end,
 }
